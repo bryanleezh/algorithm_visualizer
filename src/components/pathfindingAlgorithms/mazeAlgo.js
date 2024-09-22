9da83865104a24dab03fe45326dd9ee3f2a4555e
bryanleezh <leebryan307@gmail.com>
2024-09-22 23:50:07 +0800

    Add comments

--- src/components/pathfindingAlgorithms/mazeAlgo.js
+++ src/components/pathfindingAlgorithms/mazeAlgo.js
@@ -4,26 +4,21 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
     if (rowEnd < rowStart || colEnd < colStart) {
       return;
     }
+    // creates walls around entire grid
     if (!surroundingWalls) {
-        let startrow = parseInt(start[0])
-        let startcol = parseInt(start[1])
-        // let startcoords = [startrow,startcol]
-        let endrow = parseInt(end[0])
-        let endcol = parseInt(end[1])
-        // let endcoords = [endrow,endcol]
-        // let relevantIds = [startcoords, endcoords];
-        
+        // top & bottom walls
         for (let i = 0; i < width; i++){
             wallslist.push([0, i])
             wallslist.push([height-1, i])
-            
         }
+        // left and right walls
         for (let j = 0; j < height; j++){
             wallslist.push([j, 0])
             wallslist.push([j, width - 1])
         }
     }
     surroundingWalls = true;
+    // divide maze horizontally
     if (orientation == "horizontal") {
         let possibleRows = [];
         for (let number = rowStart; number <= rowEnd; number += 2) {
@@ -33,10 +28,13 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
         for (let number = colStart - 1; number <= colEnd + 1; number += 2) {
             possibleCols.push(number);
         }
+        // choose random row and col
         let randomRowIndex = Math.floor(Math.random() * possibleRows.length);
         let randomColIndex = Math.floor(Math.random() * possibleCols.length);
         let currentRow = possibleRows[randomRowIndex];
         let colRandom = possibleCols[randomColIndex];
+        // For each node in the grid (allnodes2), if the node is in currentRow but not in colRandom, 
+        // and if it’s within the valid column range, it is marked as a wall.
         for (let rowindex in allnodes2) {
             let rows = allnodes2[rowindex];
             for (let colindex in rows) {
@@ -51,6 +49,8 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
                 }
             }
         }
+        // If the height of the upper section (currentRow - 2) is greater than the width of the column section, 
+        // the upper section is divided horizontally again. Otherwise, it is divided vertically.
         if (currentRow-2-rowStart > colEnd-colStart ){
             recursiveDivisionMaze(wallslist, start, end, height, width, allnodes, allnodes2, rowStart, currentRow-2, colStart, colEnd, orientation, true)
         }
@@ -64,6 +64,7 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
             recursiveDivisionMaze(wallslist, start, end, height, width, allnodes, allnodes2, currentRow+2, rowEnd, colStart, colEnd, 'vertical', true)
         }
     }
+    // divide maze vertically
     else {
         let possibleCols = [];
         for (let number = colStart; number <= colEnd; number += 2) {
@@ -87,8 +88,6 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
                     if (node.status !== 'start' && node.status !== 'target') {
                         let coords = [rownum,colnum]
                         wallslist.push(coords)
-                        // allnodes[rownum][colnum].status = 'wall'
-                        // allnodes2[rownum][colnum].status = 'wall'
                     }
                 }
             }
@@ -106,21 +105,4 @@ export function recursiveDivisionMaze(wallslist, start, end, height, width, alln
             recursiveDivisionMaze(wallslist, start, end, height, width, allnodes, allnodes2, rowStart, rowEnd, currentCol+2, colEnd, orientation, true)
         }
     }
-    // console.log(wallsToAnimate)
 }
-
-    
-//       if (rowEnd - rowStart > currentCol - 2 - colStart) {
-//         recursiveDivisionMaze(board, rowStart, rowEnd, colStart, currentCol - 2, "horizontal", surroundingWalls, type);
-//       } else {
-//         recursiveDivisionMaze(board, rowStart, rowEnd, colStart, currentCol - 2, orientation, surroundingWalls, type);
-//       }
-//       if (rowEnd - rowStart > colEnd - (currentCol + 2)) {
-//         recursiveDivisionMaze(board, rowStart, rowEnd, currentCol + 2, colEnd, "horizontal", surroundingWalls, type);
-//       } else {
-//         recursiveDivisionMaze(board, rowStart, rowEnd, currentCol + 2, colEnd, orientation, surroundingWalls, type);
-//       }
-//     }
-//   };
-  
-//   module.exports = recursiveDivisionMaze;
